1fe245f7ca43a46ef2025a4cad53c72c1a2deb56
istvan-halasz <haistvan@gmail.com>
2021-03-06 18:56:53 +0200

    using default exports

--- assets/scripts/App/Tooltip.js
+++ assets/scripts/App/Tooltip.js
@@ -1,4 +1,4 @@
-import { Component } from './Component.js';
+import Component from './Component.js';
 
 export class Tooltip extends Component {
   constructor(closeNotifierFunction, text, hostElementId) {
