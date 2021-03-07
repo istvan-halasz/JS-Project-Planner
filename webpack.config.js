d370ac5a2f044e67b60400dad6421648292e9397
istvan-halasz <haistvan@gmail.com>
2021-03-07 17:55:15 +0200

    adding production configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path=require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output:{
     filename: 'app.js',
-    path: path.resolve(__dirname, 'assets', 'scripts')
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts'
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'eval-cheap-module-source-map'
 };
